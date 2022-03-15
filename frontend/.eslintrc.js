6cc84a9457cfacf260987cc87031e1386aa9ccd6
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-15 18:19:21 +0100

    Add AdList structure

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,5 +20,15 @@ module.exports = {
   ],
   rules: {
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'import/extensions': ['error', 'always', {
+      ts: 'never',
+      tsx: 'never',
+      js: 'never',
+      jsx: 'never',
+      mjs: 'never',
+    }],
+    'import/no-unresolved': 0,
   },
 };
---
+++ frontend/src/components/AdList/index.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import { ImageListItem, ImageList } from '@mui/material';
+import Ad from '../Ad';
+
+const AdList = ({ ads }) => (
+  <ImageList variant="woven" cols={3} gap={8}>
+    {ads.map(({
+      id,
+      image,
+      external,
+      rating,
+      title,
+      description,
+      price,
+      category,
+      validUntil,
+    }) => (
+      <ImageListItem key={id}>
+        <Ad
+          id={id}
+          image={image}
+          external={external}
+          rating={rating}
+          title={title}
+          description={description}
+          price={price}
+          category={category}
+          validUntil={validUntil}
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>
+);
+
+export default AdList;
