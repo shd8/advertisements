600ff91dc6890abeb02ce48faeebe818a2d4b82b
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-15 13:52:33 +0100

    Extract PORT to environment file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const jsonServer = require('json-server');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+const { PORT } = process.env || 4040;
 
+app.use(cors);
 app.use('/api', jsonServer.router('db.json'));
-
-const PORT = process.env.PORT || 4040;
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
