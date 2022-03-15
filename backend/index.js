3f25f226919f83c82b9aee3c858d53106a6c05ef
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-15 17:19:34 +0100

    Update database and add middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const jsonServer = require('json-server');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const { PORT } = process.env || 4040;
+const middlewares = jsonServer.defaults({ noCors: true });
 
-app.use(cors);
+app.use(middlewares);
 app.use('/api', jsonServer.router('db.json'));
 
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
