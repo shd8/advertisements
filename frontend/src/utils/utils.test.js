be6c134f8a4ea5e522a10f9e73a5f9340af9c6c8
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-21 09:33:15 +0100

    100% test utils

--- frontend/src/utils/utils.test.js
+++ frontend/src/utils/utils.test.js
@@ -1,8 +1,19 @@
-import { timestampToDate } from './time';
+import { timestampToDate, dateToTimestamp } from './time';
+import capitalizeFirstLetter from './texts';
 
 describe('Given utils', () => {
-  test('Should first', () => {
+  test('Should transform a timestamp to date', () => {
     const date = timestampToDate(1647438636000);
     expect(date).toBe('16/3/2022');
+  });
+
+  test('Should transform a date to a timestamp', () => {
+    const timestamp = dateToTimestamp('2022-3-21');
+    expect(timestamp).toBe(1647817200000);
+  });
+
+  test('Should capitalize only the first letter of a string', () => {
+    const result = capitalizeFirstLetter('a B C d');
+    expect(result).toBe('A B C d');
   });
 });
