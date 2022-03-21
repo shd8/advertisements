4bcf4d0f5d6619e71f1433e281de6447e275c361
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-21 09:53:20 +0100

    Test Rating component

---
+++ frontend/src/components/Rating/rating.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Rating from './index';
+
+describe('Given a Rating component', () => {
+  test('Should render 9 star icons', () => {
+    render(<Rating rating={9} />);
+
+    const numberOfStarIcons = screen.getAllByTestId('StarIcon').length;
+
+    expect(numberOfStarIcons).toBe(9);
+  });
+
+  test('Should render 6 star border icons', () => {
+    render(<Rating rating={4} />);
+
+    const numberOfStarIcons = screen.getAllByTestId('StarBorderIcon').length;
+
+    expect(numberOfStarIcons).toBe(6);
+  });
+});
